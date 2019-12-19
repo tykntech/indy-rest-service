138c10f9956181d181c589fda4715c3953602912
Eduardo Elias Saleh <du7@msn.com>
2019-12-19 16:25:54 +0100

    feat(get): get for get_txn
    Signed-off-by: Eduardo Elias Saleh <du7@msn.com>

--- index.js
+++ index.js
@@ -7,17 +7,51 @@ const app = express()
 // builderNet,  first line from: https://raw.githubusercontent.com/sovrin-foundation/sovrin/master/sovrin/pool_transactions_builder_genesis  
 const conf = process.env.GENESIS_TX || '{"reqSignature":{},"txn":{"data":{"data":{"alias":"Node1","blskey":"4N8aUNHSgjQVgkpm8nhNEfDf6txHznoYREg9kirmJrkivgL4oSEimFF6nsQ6M41QvhM2Z33nves5vfSn9n1UwNFJBYtWVnHYMATn76vLuL3zU88KyeAYcHfsih3He6UHcXDxcaecHVz6jhCYz1P2UZn2bDVruL5wXpehgBfBaLKm3Ba","blskey_pop":"RahHYiCvoNCtPTrVtP7nMC5eTYrsUA8WjXbdhNc8debh1agE9bGiJxWBXYNFbnJXoXhWFMvyqhqhRoq737YQemH5ik9oL7R4NTTCz2LEZhkgLJzB3QRQqJyBNyv7acbdHrAT8nQ9UkLbaVL9NBpnWXBTw4LEMePaSHEw66RzPNdAX1","client_ip":"127.0.0.1","client_port":9702,"node_ip":"127.0.0.1","node_port":9701,"services":["VALIDATOR"]},"dest":"Gw6pDLhcBcoQesN72qfotTgFa7cbuqZpkX3Xo6pLhPhv"},"metadata":{"from":"Th7MpTaRZVRYnPiabds81Y"},"type":"0"},"txnMetadata":{"seqNo":1,"txnId":"fea82e10e894419fe2bea7d96296a6d46f50f93f9eeda954ec461b2ed2950b62"},"ver":"1"}';
 
+const getLedger = function (something) {
+  switch (something.toString().toLowerCase()) {
+    case '1':    
+      return 1;
+    case 'pool':
+      return 0;
+    case 'domain':
+      return 1;
+    default:
+      return 1;    
+  }
+}
+
 app.use(bodyParser.json());
 
-app.post('/tx', async (req, res) => {
-  const ledger = zmqlib.Wrap(zmqlib.ParseGenesisTx(conf));
+app.get('/tx/:ledger?/:number', async (req, res) => {    
+  const parsedConf = await zmqlib.ParseGenesisTx(conf);  
+  const ledger = zmqlib.Wrap(parsedConf);  
+  let resp;  
+  try {
+    resp = await ledger.send({
+      "operation": {
+        "type": "3",
+        "ledgerId": getLedger(req.params.ledger || 1), 
+        "data": parseInt(req.params.number)
+      },
+      "identifier": "LibindyDid211111111111", 
+      "protocolVersion": 2
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+  res.status(200).json(resp.result.data);
+});
+
+app.post('/tx', async (req, res) => {    
+  const parsedConf = await zmqlib.ParseGenesisTx(conf);  
+  const ledger = zmqlib.Wrap(parsedConf);  
   let resp;  
   try {
     resp = await ledger.send(req.body);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
-  res.status(200).json(resp);
+  res.status(200).json(resp.result.data);
 });
 
 const port = process.env.PORT || 3000
