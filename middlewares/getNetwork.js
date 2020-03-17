2003a7296bd7a4b22aa75b85370fd1743b64342a
eduelias <du7@msn.com>
2020-03-17 16:18:13 +0100

    fix(console): removing console log
    Signed-off-by: eduelias <du7@msn.com>

--- middlewares/getNetwork.js
+++ middlewares/getNetwork.js
@@ -1,22 +1,23 @@
 const zmqlib = require('@tykntech/indy-zmq-lib');
 
 const networks = {
-    sovbuilder: require('../networks/sovbuilder.json'),
-    sovstaging: require('../networks/sovstaging.json'),
-    sovmain: require('../networks/sovmain.json'),
-    sovlocal: require('../networks/sovlocal.json')
-}
+  sovbuilder: require('../networks/sovbuilder.json'),
+  sovstaging: require('../networks/sovstaging.json'),
+  sovmain: require('../networks/sovmain.json'),
+  sovlocal: require('../networks/sovlocal.json'),
+};
 
-exports.getNetwork =
-    async function (req, res, next) {
-        const { network } = req.params
-        console.log(`Using ${network} as network.`)
+exports.getNetwork = async function(req, res, next) {
+  const { network } = req.params;
 
-        if (!networks[network]) {
-            return res.status(404).json({ message: `The network ${network} is not available in this API.` })
-        }
+  if (!networks[network]) {
+    return res
+      .status(404)
+      .json({ message: `The network ${network} is not available in this API.` });
+  }
 
-        req.parsedConfig = await Promise.all(networks[network].map(async net => await zmqlib.ParseGenesisTx(net)))
-
-        next();
-    }
+  req.parsedConfig = await Promise.all(
+    networks[network].map(async net => await zmqlib.ParseGenesisTx(net))
+  );
+  next();
+};
